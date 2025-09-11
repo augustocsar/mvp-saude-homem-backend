3e53aa59f5419689856ec17e81d5cc5c2ba81218
Augusto César Farias Carvalho <114708900+augustocsar@users.noreply.github.com>
2025-09-11 20:53:53 -0300

    fix: adiciona a rota de autenticação

--- app/routes/router.py
+++ app/routes/router.py
@@ -1,10 +1,10 @@
 from fastapi import APIRouter
 from app.controllers.rss_controller import router as rss_router
-
+from app.controllers.user_controller import router as user_router
 
 # Router principal que agrega todos os sub-routers
 router = APIRouter()
 
 # Incluir routers
 router.include_router(rss_router)
-
+router.include_router(user_router)  # ← ADICIONAR ESTA LINHA
